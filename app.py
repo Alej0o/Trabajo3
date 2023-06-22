7218874adfbf66cf74f2d3453768c5a71c00f9d7
Alej0o <137226603+Alej0o@users.noreply.github.com>
2023-06-21 19:22:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,1 @@
-import os
-from flask import Flask, flash, url_for
-from flask import render_template, request, redirect, session
-import psycopg2
-from datetime import datetime
-from flask import send_from_directory
-#descarga
-from flask import send_file
-import uuid
-from werkzeug.utils import secure_filename
-from os import path
 
-
-app=Flask(__name__)
-app.secret_key="steveen"
-
-
-connection=psycopg2.connect(dbname="proysemilleros", user="postgres", password="st3v3")
-hola
